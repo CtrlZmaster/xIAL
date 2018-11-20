6f65e4c69a1df3ee2616660a81122563cdca6e3b
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2018-11-20 21:31:50 +0100

    Vytvoreny CLion projekt

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * Project: IAL ...
+ * File: main.c
+ * Title: 7. Minimum weight spanning tree
+ * Description: ...
+ * Author: František Balázsy (xbalaz08@feec.vutbr.cz),
+ *         Michal Pospíšil (xpospi95@stud.fit.vutbr.cz),
+ *         Nikola Timková (xtimko01@stud.fit.vutbr.cz)
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}

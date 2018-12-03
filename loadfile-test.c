ea782a3b895ca59314afaf1c7516fb6bcc947359
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2018-12-03 16:03:34 +0100

    Pridané zatvorenie súboru a úprava odsadenia

--- loadfile-test.c
+++ loadfile-test.c
@@ -9,39 +9,41 @@
 // DO NOT SUBMIT!!!
 
 int main(int argc, char *argv[]) {
-    int err_code = 0;
-    // Resets locale from C standard to environment locale (particularly ASCII to UTF-8)
-    setlocale(LC_ALL, "");
+  int err_code = 0;
+  // Resets locale from C standard to environment locale (particularly ASCII to UTF-8)
+  setlocale(LC_ALL, "");
 
-    FILE * file_ptr;
-    if((file_ptr = fopen(argv[1], "r")) == NULL) {
-        fprintf(stderr, "ERROR: File with name \"%s\" couldn't be opened.\n", argv[1]);
-        return -1;
-    }
+  FILE * file_ptr;
+  if((file_ptr = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "ERROR: File with name \"%s\" couldn't be opened.\n", argv[1]);
+    return -1;
+  }
 
-    // Fill the matrix
-    adj_matrix_t *matrix = malloc(sizeof(adj_matrix_t));
-    if(matrix == NULL) {
-        return -5;
-    }
+  // Fill the matrix
+  adj_matrix_t *matrix = malloc(sizeof(adj_matrix_t));
+  if(matrix == NULL) {
+    return -5;
+  }
 
-    err_code = loadToMatrix(file_ptr, matrix);
-    if(err_code) {
-        loaderror(err_code);
-        return err_code;
-    }
-
-    for(uint8_t i = 0; i < matrix->vertices; i++) {
-        wprintf(L"%ls, ",matrix->vertex_names[i]);
-    }
-    wprintf(L"\n");
-    for(uint8_t i = 0; i < matrix->vertices; i++) {
-        for(uint8_t j = 0; j < matrix->vertices; j++) {
-            wprintf(L"%ld,",matrix->matrix[i][j]);
-        }
-        wprintf(L"\n");
-    }
-
-    free_matrix(matrix);
+  err_code = loadToMatrix(file_ptr, matrix);
+  if(err_code) {
+    loaderror(err_code);
     return err_code;
+  }
+
+  fclose(file_ptr);
+  
+  for(uint8_t i = 0; i < matrix->vertices; i++) {
+    wprintf(L"%ls, ",matrix->vertex_names[i]);
+  }
+  wprintf(L"\n");
+  for(uint8_t i = 0; i < matrix->vertices; i++) {
+    for(uint8_t j = 0; j < matrix->vertices; j++) {
+      wprintf(L"%ld,",matrix->matrix[i][j]);
+      }
+    wprintf(L"\n");
+  }
+
+  free_matrix(matrix);
+  return err_code;
 }
